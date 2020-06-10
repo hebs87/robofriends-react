0bacaa96a22bc7fb7560d11463063383aaa1df56
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-10 22:06:43 +0100

    Rendered SearchBox in App, converted App to class component and passed robots data as props to CardList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {Fragment} from "react";
+import React, {Component} from "react";
+import robots from "./data/robots";
 import CardList from "./components/CardList";
+import SearchBox from "./components/SearchBox";
 
-const App = () => {
-  return (
-    <Fragment>
-      <h1>RoboFriends</h1>
-      <CardList/>
-    </Fragment>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox/>
+        <CardList robots={robots}/>
+      </div>
+    )
+  }
 }
 
 export default App;
