cf4ed9daa703a11ae3fa69328a1503cbb5e84073
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-10 22:33:54 +0100

    Imported font and applied custom styles to project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import robots from "./data/robots";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -25,7 +26,7 @@ class App extends Component {
 
     return (
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox onSearchChange={this.onSearchChange}/>
         <CardList robots={filteredRobots}/>
       </div>
