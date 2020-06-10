c04bf81ad4ca88883742ba86ba6d0c5d550e792f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-10 21:58:34 +0100

    Created App component and moved CardList into it

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from "react";
+import CardList from "./components/CardList";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>RoboFriends</h1>
+      <CardList/>
+    </Fragment>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import CardList from "./components/CardList";
+import App from "./App";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
