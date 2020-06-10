b7cab5d0cbf1183884538504135bc4b2c5424e10
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-10 21:43:40 +0100

    Imported robots data and rendered in cards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import 'tachyons';
+import robots from "./robots";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {
+      robots &&
+      robots.map(robot => (
+        <Card key={robot.id} robot={robot}/>
+      ))
+    }
   </React.StrictMode>,
   document.getElementById('root')
 );
