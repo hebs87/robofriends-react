26406c9492deb967d4a5344328c27c1641b4756b
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-11 16:27:53 +0100

    Pulled store into index.js and wrapped App with Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Redux imports
+import {Provider} from 'react-redux'
+import {createStore} from "redux";
 import './index.css';
 import 'tachyons';
 import App from "./containers/App";
+import {searchRobots} from "./redux/reducers";
 import * as serviceWorker from './serviceWorker';
 
+// Create store and pass into Provider - this is normally the root reducer
+const store = createStore(searchRobots);
+
+// Wrap app in Provider to give it access to the store
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
