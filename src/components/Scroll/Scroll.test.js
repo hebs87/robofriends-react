02d85a6c5ace2dc86d3b48c87da9f9be0a40f856
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-03-11 22:46:11 +0000

    Created test suite for Scroll component

---
+++ src/components/Scroll/Scroll.test.js
@@ -0,0 +1,11 @@
+import {shallow} from 'enzyme';
+import Scroll from "./Scroll";
+
+describe('Scroll component test suite', () => {
+  it('expect to render Scroll component', () => {
+    // shallow renders just the target component and not any nest components that are rendered within it
+    // toMatchSnapshot() takes a snapshot of the component and tests it is rendering as it should - any change to the
+    // component will then cause the test to fail, as it will no longer match the snapshot
+    expect(shallow(<Scroll/>)).toMatchSnapshot();
+  });
+});
