213e29e0e1e6cbbc4f11cd0fb9d62034e6a39615
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-03-11 22:59:09 +0000

    Added props to test suite components

--- src/components/Scroll/Scroll.test.js
+++ src/components/Scroll/Scroll.test.js
@@ -6,6 +6,6 @@ describe('Scroll component test suite', () => {
     // shallow renders just the target component and not any nest components that are rendered within it
     // toMatchSnapshot() takes a snapshot of the component and tests it is rendering as it should - any change to the
     // component will then cause the test to fail, as it will no longer match the snapshot
-    expect(shallow(<Scroll/>)).toMatchSnapshot();
+    expect(shallow(<Scroll children/>)).toMatchSnapshot();
   });
 });
